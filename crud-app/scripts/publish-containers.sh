bd88f99a9ccddb031cca0aa97f033fdbb6fa9103
Touko Peltomaa <touko.peltomaa@gmail.com>
2025-01-18 13:04:50 +0200

    fix: crud container tagging

--- crud-app/scripts/publish-containers.sh
+++ crud-app/scripts/publish-containers.sh
@@ -1,6 +1,6 @@
 docker buildx build --platform linux/amd64,linux/arm64 -t toukop/crud-app:latest .
 docker push toukop/crud-app:latest
 
-docker tag toukop/crud-app:latest europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-backend:latest
+docker tag toukop/crud-app:latest europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-app:latest
 
 docker push europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-app:latest
