af8992562216e774b698bf784b250ee7ba8a50f5
Touko Peltomaa <touko.peltomaa@gmail.com>
2025-01-18 13:01:27 +0200

    feat: publish crud app to gcp as well

--- crud-backend/scripts/publish-containers.sh
+++ crud-backend/scripts/publish-containers.sh
@@ -2,3 +2,9 @@ docker buildx build --platform linux/amd64,linux/arm64 -t toukop/crud-backend:la
 docker buildx build --platform linux/amd64,linux/arm64 -t toukop/crud-backend-worker:latest -f Dockerfile-worker .
 docker push toukop/crud-backend:latest
 docker push toukop/crud-backend-worker:latest
+
+docker tag toukop/log-output-worker:latest europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-backend:latest
+docker tag toukop/log-output-web:latest europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-backend-worker:latest
+
+docker push europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-backend:latest
+docker push europe-north1-docker.pkg.dev/nomadic-charge-446815-j2/kubernetes-course/crud-backend-worker:latest
