2cae9f11d19b0bc5b7728f33aa18aeb718816afc
Touko Peltomaa <touko.peltomaa@gmail.com>
2025-01-26 14:36:53 +0200

    fix: Fix created_at

--- crud-backend/models/Task.go
+++ crud-backend/models/Task.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Task struct {
-	ID        int       `json:"id"`
-	Task      string    `json:"task"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	IsDone    bool      `json:"is_done"`
+	ID        int        `json:"id"`
+	Task      string     `json:"task"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	IsDone    bool       `json:"is_done"`
 }
